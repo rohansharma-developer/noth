f4e0be5c0bc23bed030e420cd9da1a9dbbb9c5f0
Rohan Sharma <107614947+rohansharma-developer@users.noreply.github.com>
2023-12-30 15:44:40 +0530

    Update wow.sh

--- wow.sh
+++
@@ -1,5 +0,0 @@
-curl -L --output cloudflared.deb https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb && 
-
-sudo dpkg -i cloudflared.deb && 
-
-sudo cloudflared service install eyJhIjoiMTEyMzA0YjZjMWM0Yzc0MDQyODFkNjE3NWZkYzk2NjciLCJ0IjoiNGM4NjExMzAtZjI4My00NmFmLWI0ZDMtYTZlNTg0YTMzMDUzIiwicyI6Ik5HRXlPV1E1TW1NdE9EWTROUzAwT1RRM0xUa3lZMkV0T0ROaVptVXhZekJoTmpZMiJ9
